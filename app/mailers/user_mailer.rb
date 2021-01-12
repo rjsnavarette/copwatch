1290236408f7f258c8c170a845be38a503079ee8
Kimson Wong <kimsonwong@gmail.com>
2021-01-12 11:51:17 +0800

    fix error for user mailer email verification

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ApplicationMailer
   def verification_email
     @user = params[:user]
-    @link = "copwatch://open?action=0&code=#{@user.verification_code}"
+    @link = "copwatch://open?action=0&token=#{@user.verification_token}"
 
     mail(to: @user.email, subject: 'CopWatch Email Verification')
   end
 
   def forgot_password_email
     @user = params[:user]
-    @link = "copwatch://open?action=1&code=#{@user.password_reset_token}"
+    @link = "copwatch://open?action=1&token=#{@user.password_reset_token}"
 
     mail(to: @user.email, subject: 'Forgot Password')
   end
