75216a6854cab0e88ae70dd1eae563d6efaea56e
Kimson Wong <kimsonwong@gmail.com>
2021-01-12 10:01:41 +0800

    add mailer config for staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -89,6 +89,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # configure mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => Rails.application.credentials.dig(:mailer, :email),
+    :password             => Rails.application.credentials.dig(:mailer, :password),
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+}
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
