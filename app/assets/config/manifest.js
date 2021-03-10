39c3b707d3b6bee176c7eb26e14fce0fa7bf0b31
Kimson Wong <kimsonwong@gmail.com>
2021-03-10 20:45:20 +0800

    replaced existing wysiwyg form add resources for new wysiwyg form

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,5 @@
 //= link_directory ../../../vendor/assets/stylesheets .css
 //= link_directory ../../../vendor/assets/javascript .js
 //= link_directory ../../../vendor/assets/fonts
+//= link_directory ../../../vendor/assets/stylesheets/font-awesome/css
+//= link_directory ../../../vendor/assets/stylesheets/font-awesome/fonts
--- app/javascript/packs/admin/pages.js
+++ app/javascript/packs/admin/pages.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  new FroalaEditor('#page_content', {})
+  // new FroalaEditor('#page_content', {})
+  $("#page_content").wysiwyg();
+  $(".wysiwyg-editor").hide();
 
   $("form .edit").on("click", function(e) {
     e.preventDefault();
@@ -9,13 +11,15 @@ $(document).ready(function() {
       $("#page_title, #page_content").attr("readonly", false);
       $("form .save").removeClass("hide");
       $(this).text("Cancel");
-      $("form textarea.no-resize").addClass("hide");
+      $("form textarea.no-resize").hide();
+      $("form .wysiwyg-editor").show();
       $("form .fr-box").css("display", "block");
     } else {
       $("#page_title, #page_content").attr("readonly", true);
       $("form .save").addClass("hide");
       $(this).text("Edit")
-      $("form textarea.no-resize").removeClass("hide");
+      $("form textarea.no-resize").show();
+      $("form .wysiwyg-editor").hide();
       $("form .fr-box").css("display", "none");
     }
   })
